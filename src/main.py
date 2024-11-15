b9bd34c2c4e2be249b3be6f178308920af2cbc40
parth-gr <partharora1010@gmail.com>
2024-11-16 00:38:15 +0530

    add wandb metrics to the project
    Signed-off-by: parth-gr <partharora1010@gmail.com>

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 from sklearn.neural_network import MLPRegressor
 import pickle
-
+import wandb
 
 def retrieve_and_clean_data():
-    filename = "data/train.csv"
+    filename = "../data/train.csv"
     data_df = pd.read_csv(filename)
     # data_df["WillFloodingOccur"] = np.where(data_df["FloodProbability"] < 0.5, 0, 1)
 
@@ -24,13 +24,22 @@ def predict_flood_prob(request):
 
 
 if __name__ == '__main__':
+    # start a new wandb run to track this script
+    wandb.init(project="flood-detection")
+      
     X, y = retrieve_and_clean_data()
+    wandb.log({"data_size": len(X), "feature_count": X.shape[1]})
 
     # Create model and save
     model = MLPRegressor(random_state=1, hidden_layer_sizes=(25,)).fit(X, y)
     print("Model trained successful!")
+    
+    train_score = model.score(X, y)  # R^2 score
+    wandb.log({"train_score": train_score})
+    
     with open('model.pkl', 'wb') as f:
         pickle.dump(model, f)
-
     print("Model saved to disk!")
-
+    wandb.save("model.pkl")
+    
+    wandb.finish()
